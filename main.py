58f53adfe017f8cab4169d49508d09d9d4281464
JackAZ <13146653740@163.com>
2020-02-02 14:34:47 +0800

    update code

--- main.py
+++ main.py
@@ -17,36 +17,7 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
-
-# Define a Convolutional Neural Network
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# Copy the neural network from the Neural Networks section before and modify it to
-# take 3-channel images (instead of 1-channel images as it was defined).
-
-# out_size = (in_size+2*pad-filter)/strides+1
-# cifar 32*32 , 50000 training images and 10000 testing images
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(32 * 8 * 8, 128)
-        self.fc2 = nn.Linear(128, 10)
-        self.fc3 = nn.Linear(10, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 32 * 8 * 8)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        # x = F.relu(self.fc2(x))
-        # x = self.fc3(x)
-        return x
-
+import net_structure as mNet
 
 if __name__ == '__main__':
     ########################################################################
@@ -68,7 +39,7 @@ if __name__ == '__main__':
 
     ########################################################################
     # 2. Define a Convolutional Neural Network
-    net = Net()
+    net = mNet.original()
 
     ########################################################################
     # 3. Define a Loss function and optimizer
@@ -78,7 +49,7 @@ if __name__ == '__main__':
     ########################################################################
     # 4. Train the network
 
-    for epoch in range(10):  # loop over the dataset multiple times
+    for epoch in range(6):  # loop over the dataset multiple times
 
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
@@ -112,45 +83,6 @@ if __name__ == '__main__':
     ########################################################################
     # 5. Test the network on the test data
 
-    dataiter = iter(testloader)
-    images, labels = dataiter.next()
-
-    # print images
-    # functions to show an image
-    # def imshow(img):
-    #     img = img / 2 + 0.5  # unnormalize
-    #     npimg = img.numpy()
-    #     plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    #     plt.show()
-    # imshow(torchvision.utils.make_grid(images))
-    # print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-    ########################################################################
-    # Next, let's load back in our saved model (note: saving and re-loading the model
-    # wasn't necessary here, we only did it to illustrate how to do so):
-
-    net = Net()
-    net.load_state_dict(torch.load(PATH))
-
-    ########################################################################
-    # Okay, now let us see what the neural network thinks these examples above are:
-
-    outputs = net(images)
-
-    ########################################################################
-    # The outputs are energies for the 10 classes.
-    # The higher the energy for a class, the more the network
-    # thinks that the image is of the particular class.
-    # So, let's get the index of the highest energy:
-    _, predicted = torch.max(outputs, 1)
-
-    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
-
-    ########################################################################
-    # The results seem pretty good.
-    #
-    # Let us look at how the network performs on the whole dataset.
-
     correct = 0
     total = 0
     with torch.no_grad():
@@ -167,10 +99,6 @@ if __name__ == '__main__':
     ########################################################################
     # That looks way better than chance, which is 10% accuracy (randomly picking
     # a class out of 10 classes).
-    # Seems like the network learnt something.
-    #
-    # Hmmm, what are the classes that performed well, and the classes that did
-    # not perform well:
 
     class_correct = list(0. for i in range(10))
     class_total = list(0. for i in range(10))
