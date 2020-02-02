259a5d87710c3e10dfd77263303cdf863fd91c10
JackAZ <13146653740@163.com>
2020-02-02 14:29:41 +0800

    update main code

---
+++ net_structure.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Define a Convolutional Neural Network
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Copy the neural network from the Neural Networks section before and modify it to
+# take 3-channel images (instead of 1-channel images as it was defined).
+
+# out_size = (in_size+2*pad-filter)/strides+1
+# cifar 32*32 , 50000 training images and 10000 testing images
+
+class original(nn.Module):
+    def __init__(self):
+        super(original, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(32 * 8 * 8, 128)
+        self.fc2 = nn.Linear(128, 10)
+        self.fc3 = nn.Linear(10, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 32 * 8 * 8)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        # x = F.relu(self.fc2(x))
+        # x = self.fc3(x)
+        return x
